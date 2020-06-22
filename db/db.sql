223c7270e93758902f5044f1ff3e311aab5737e1
TheoSabot <theo@virutalbox.com>
2020-06-22 17:34:50 +0200

    added sql model for user and business

---
+++ db/db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE gomail;
+
+CREATE TABLE business (
+    business_id serial PRIMARY KEY,
+    name VARCHAR (64) NOT NULL
+);
+
+CREATE TABLE users (
+    user_id bigserial PRIMARY KEY,
+    email VARCHAR (128) NOT NULL,
+    password VARCHAR (256) NOT NULL,
+    enterprise_id serial
+);
