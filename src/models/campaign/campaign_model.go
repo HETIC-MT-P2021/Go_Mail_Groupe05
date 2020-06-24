505da8fdc1423c8438934dc96cd2137ca325da25
Jean-Jacques <akakpo.jeanjacques@gmail.com>
2020-06-24 15:54:31 +0200

    🔀 Merge branch and add campaign model and database structure

---
+++ src/models/campaign/campaign_model.go
@@ -0,0 +1,9 @@
+package campaignmodel
+
+// Campaign Represents a mail campaign
+type Campaign struct {
+	id              uint
+	broadcastListID uint
+	title           string
+	body            string
+}
