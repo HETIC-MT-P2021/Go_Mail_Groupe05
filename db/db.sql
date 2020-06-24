355c4d79d524b119b925cae499fadf491f00e0fc
TheoSabot <theo@virutalbox.com>
2020-06-24 16:54:44 +0200

    added db schema

--- db/db.sql
+++ db/db.sql
@@ -6,18 +6,35 @@ CREATE TABLE business
   name VARCHAR (64) NOT NULL
 );
 
-CREATE TABLE users
-(
-  user_id bigserial PRIMARY KEY,
-  email VARCHAR (128) NOT NULL,
-  password bytea NOT NULL,
-  enterprise_id serial
+CREATE TABLE users (
+    user_id bigserial PRIMARY KEY,
+    email VARCHAR (128) NOT NULL,
+    password bytea NOT NULL,
+    business_id serial
 );
 
-CREATE TABLE campaign
-(
-  campaign_id serial PRIMARY KEY,
-  broadcastListId serial NOT NULL,
-  title VARCHAR (128) NOT NULL,
-  body TEXT NOT NULL,
+CREATE TABLE campaign (
+    campaign_id serial PRIMARY KEY,
+    name VARCHAR (128) NOT NULL,
+    mailing_list_id serial NOT NULL,
+    business_id serial
+);
+
+CREATE TABLE mailing_list (
+    mailing_list_id serial NOT NULL,
+    name VARCHAR (128) NOT NULL,
+    business_id serial NOT NULL
+);
+
+CREATE TABLE mailing_list_customer_assoc (
+    mailing_list_id serial NOT NULL,
+    customer_id serial NOT NULL
+);
+
+CREATE TABLE customer (
+    customer_id serial NOT NULL,
+    email VARCHAR (128) NOT NULL,
+    name VARCHAR (128) NOT NULL,
+    surname VARCHAR (128) NOT NULL,
+    business_id serial NOT NULL
 );
