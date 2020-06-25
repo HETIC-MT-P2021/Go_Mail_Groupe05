61431e89d669ec0fd8e04d3d579660b7f87a7167
myouuu <meriem.mrabent@hetic.net>
2020-06-25 14:51:29 +0200

    :card_file_box: update DB SQL

--- db/db.sql
+++ db/db.sql
@@ -10,31 +10,31 @@ CREATE TABLE users (
     user_id bigserial PRIMARY KEY,
     email VARCHAR (128) NOT NULL,
     password bytea NOT NULL,
-    business_id serial
+    business_id serial references business(business_id)
 );
 
 CREATE TABLE campaign (
-    campaign_id serial PRIMARY KEY,
+    campaign_id serial PRIMARY KEY NOT NULL,
     name VARCHAR (128) NOT NULL,
-    mailing_list_id serial NOT NULL,
-    business_id serial
+    mailing_list_id serial NOT NULL references mailing_list(mailing_list_id),
+    business_id serial references business(business_id)
 );
 
 CREATE TABLE mailing_list (
-    mailing_list_id serial NOT NULL,
+    mailing_list_id serial PRIMARY KEY NOT NULL,
     name VARCHAR (128) NOT NULL,
-    business_id serial NOT NULL
+    business_id serial NOT NULL references business(business_id)
 );
 
 CREATE TABLE mailing_list_customer_assoc (
-    mailing_list_id serial NOT NULL,
-    customer_id serial NOT NULL
+    mailing_list_id serial NOT NULL references mailing_list(mailing_list_id),
+    customer_id serial NOT NULL references customer(customer_id)
 );
 
 CREATE TABLE customer (
-    customer_id serial NOT NULL,
+    customer_id serial PRIMARY KEY NOT NULL,
     email VARCHAR (128) NOT NULL,
     name VARCHAR (128) NOT NULL,
     surname VARCHAR (128) NOT NULL,
-    business_id serial NOT NULL
+    business_id serial NOT NULL references business(business_id)
 );
